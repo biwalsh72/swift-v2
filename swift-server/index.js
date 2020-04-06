5894c0378c8ce45198be725bc968d2e380d0e7c1
biwalsh72 <biwalsh72@gmail.com>
2020-04-06 17:18:03 -0400

    basic swift backend server implementation

---
+++ swift-server/index.js
@@ -0,0 +1,51 @@
+/*jshint esversion: 6 */
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const config = require('config')
+const morgan = require('morgan')
+const server = require('http').Server(app)
+const socket = require('socket.io')(server)
+const path = require('path')
+
+// Middlewares
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(cors())
+
+const socketConnection = require('./socket')
+mongoose
+  .connect(
+    config.get('MONGODB_URI'),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err, db) => {
+      console.log(`Connected to Swift MongoDB...`)
+
+      socket.on('connection', socketConnection)
+    }
+  )
+  .catch(er => console.error(er))
+
+// Routes
+const user = require('./routes/user')
+const chat = require('./routes/chat')
+
+// handle users and chats from the app
+app.use('/api/users', user)
+app.use('/api/chats', chat)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.use('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
+const port = process.env.PORT || 4000
+server.listen(port, () => console.log(`Listening to port ${port}...`))
---
+++ swift-server/models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+// create schema for users added to users collection
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    minLength: 2,
+    maxLength: 12
+  },
+  bio: {
+    type: String,
+    maxLength: 25,
+    default: '...'
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  salt: {
+    type: String
+  },
+  privateKeyCipher: {
+    type: String
+  },
+  publicKey: {
+    type: String
+  },
+  pbkHash: {
+    type: String
+  },
+  status: {
+    type: String,
+    default: ''
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+const generateToken = auth => {
+  return jwt.sign({ auth }, config.get('jwtKey'))
+}
+
+const validate = user => {
+  const schema = {
+    username: Joi.string()
+      .required()
+      .min(2)
+      .max(12),
+    bio: Joi.string()
+      .default('...')
+      .max(25),
+    password: Joi.string().required(),
+    salt: Joi.string(),
+    privateKeyCipher: Joi.string(),
+    publicKey: Joi.string(),
+    pbkHash: Joi.string(),
+    status: Joi.string().default('')
+  }
+
+  return Joi.validate(user, schema)
+}
+
+module.exports = {
+  User,
+  validate,
+  generateToken
+}
